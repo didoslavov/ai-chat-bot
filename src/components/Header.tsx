6b02f3f2b28dcef447d7c87ae3ccf78fb631597b
didoslavov <didkoslawow@gmail.com>
2024-05-09 01:02:38 +0300

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/components/Logo";
+
+function Header() {
+  return (
+    <header className="flex items-center border-b-2 border-primary">
+      <Logo />
+    </header>
+  );
+}
+
+export default Header;
