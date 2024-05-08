ba28058ca0efd6302becbb1159252243e70f9d8a
didoslavov <didkoslawow@gmail.com>
2024-05-09 01:01:25 +0300

    add icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import imgIcon from "/public/img-icon.png";
+import micIcon from "/public/mic-icon.png";
+import sendIcon from "/public/send-icon.png";
+import { IconType } from "@/types/IconType";
+
+const icons = {
+  img: imgIcon,
+  mic: micIcon,
+  send: sendIcon,
+};
+
+function Logo({ type }: { type: IconType }) {
+  return (
+    <div className="mx-auto cursor-pointer">
+      <Image className="max-w-5" src={icons[type]} alt="Chat icon" />
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/types/IconType.ts
@@ -0,0 +1,1 @@
+export type IconType = "img" | "mic" | "send";
