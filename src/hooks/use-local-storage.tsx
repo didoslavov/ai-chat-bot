34d8798151abc96ce4ee2e2bdbcf1e73e01c0d96
didoslavov <didkoslawow@gmail.com>
2024-05-09 22:19:59 +0300

    add custom hook to manage and persist messages and suggestions state

---
+++ src/hooks/use-local-storage.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+type SetValue<T> = (value: T | ((prev: T) => T)) => void;
+
+function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
+  const isClient = typeof window !== "undefined";
+
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      if (isClient) {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      } else {
+        return initialValue;
+      }
+    } catch (error) {
+      console.error("Error parsing local storage value:", error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem(key, JSON.stringify(storedValue));
+    }
+  }, [storedValue, key, isClient]);
+
+  return [storedValue, setStoredValue] as const;
+}
+
+export default useLocalStorage;
