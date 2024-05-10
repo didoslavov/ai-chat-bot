63c63cd42b75f467a8d754e4c2b13551d5e4e857
didoslavov <didkoslawow@gmail.com>
2024-05-10 19:13:27 +0300

    add utility to transform img to base64

---
+++ src/utils/file-to-base64.ts
@@ -0,0 +1,10 @@
+export function convertToBase64(
+  file: File,
+): Promise<string | ArrayBuffer | null> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
